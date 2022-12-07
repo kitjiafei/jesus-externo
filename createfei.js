909a264dc3735fc0688cb84ee932aabf72f533ef
kitjiafei <116738996+kitjiafei@users.noreply.github.com>
2022-12-07 11:13:48 -0300

    Add files via upload

---
+++ createfei.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+
+fs.appendFile('myjiafile.txt', 'Mei tseru chan twa jiafei products', function(err){
+    if (err) throw err;
+    console.log('lêer geskep! *jiafei scream* ערכת סקס הומואים');
+});
